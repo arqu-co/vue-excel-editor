902e210e20a99e55b4ab6afe7c45ea0c2d075ea7
Michael Cheung <mcheung@me.com>
2022-11-07 11:07:15 -0800

    set setting on mouseUp

--- src/VueExcelEditor.vue
+++ src/VueExcelEditor.vue
@@ -538,7 +538,7 @@ export default {
         this.lazy(() => {
           const setting = this.getSetting()
           if (this.remember) localStorage[window.location.pathname + '.' + this.token] = JSON.stringify(setting)
-          this.$emit('setting', setting)
+          // this.$emit('setting', setting)
         })
       },
       deep: true
@@ -1550,6 +1550,7 @@ export default {
       window.removeEventListener('mouseup', this.colSepMouseUp)
       const setting = this.getSetting()
       if (this.remember) localStorage[window.location.pathname + '.' + this.token] = JSON.stringify(setting)
+      this.setting = setting
       this.$emit('setting', setting)
     },
 
